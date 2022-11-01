cfc0d0b831d175b249396081b740b352137ffad8
Nixuge <mail@nixuge.me>
2022-11-01 22:18:35 +0000

    CSS fix & js removed (unneeded)

--- static/js/index.js
+++
@@ -1,9 +0,0 @@
-// https://stackoverflow.com/q/56434924
-//fuck safari once again
-window.addEventListener('load', function () {
-    var xs = document.querySelectorAll('.section');
-    [].slice.call(xs).forEach(function (x) {
-      // doing this via CSS has no effect
-      x.style.setProperty('display', 'block');
-    });
-  });
